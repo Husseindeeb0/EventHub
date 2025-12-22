657e9b8996df114d20924a9bd6896d490176763b
Hussein <deebh776@gmail.com>
2025-12-22 22:54:26 +0200

    Fix deployment error

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -2,14 +2,20 @@ import imagekit from "@/lib/imagekit";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    try {
-        const authenticationParameters = imagekit.getAuthenticationParameters();
-        return NextResponse.json(authenticationParameters);
-    } catch (error) {
-        console.error("ImageKit Auth Error:", error);
-        return NextResponse.json(
-            { error: "ImageKit Authentication Failed" },
-            { status: 500 }
-        );
+  try {
+    if (!imagekit) {
+      return NextResponse.json(
+        { error: "ImageKit not configured on server" },
+        { status: 500 }
+      );
     }
+    const authenticationParameters = imagekit.getAuthenticationParameters();
+    return NextResponse.json(authenticationParameters);
+  } catch (error) {
+    console.error("ImageKit Auth Error:", error);
+    return NextResponse.json(
+      { error: "ImageKit Authentication Failed" },
+      { status: 500 }
+    );
+  }
 }
